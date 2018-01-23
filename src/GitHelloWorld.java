13ed28c7e0cb1e34bb715c20a52d2ba6af17e2a8
alexbrozovich <alexbrozovich@gmail.com>
2018-01-23 13:38:58 -0500

    first commit helloworld

---
+++ src/GitHelloWorld.java
@@ -0,0 +1,8 @@
+//This is a simple hello world program that will be used to push to GitHub
+public class GitHelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
